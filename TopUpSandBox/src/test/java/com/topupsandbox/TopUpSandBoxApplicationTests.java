5b9f05733ad4a27922af857f2ec89bf455266fdb
Md. Faiz Shiraji <faizshiraji@gmail.com>
2021-02-12 07:54:42 +0600

    First Commit

---
+++ TopUpSandBox/src/test/java/com/topupsandbox/TopUpSandBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topupsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopUpSandBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
