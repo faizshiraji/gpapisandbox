5bd2915e81d6b87c82d730c9c7467982f88a51d1
Md. Faiz Shiraji <faizshiraji@gmail.com>
2021-02-20 06:39:11 +0600

    Keep going ....

---
+++ TopUpSandBox/src/main/java/com/topupsandbox/services/GPRequestServiceImpl.java
@@ -0,0 +1,49 @@
+package com.topupsandbox.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.topupsandbox.entities.request.GPRequest;
+import com.topupsandbox.repos.GPRequestRepo;
+
+@Service
+public class GPRequestServiceImpl implements GPRequestService {
+
+	@Autowired
+	public GPRequestRepo gpRequestRepo;
+	
+	@Override
+	public List<GPRequest> getGpRequests() {
+		return gpRequestRepo.findAll();
+	}
+
+	@Override
+	public GPRequest getGpRequest(int gpRequestId) {
+		return gpRequestRepo.findById(gpRequestId).get();
+	}
+
+	@Override
+	public GPRequest getGpRequestByTxnId(String gpextrefnum) {
+		return gpRequestRepo.findByextrefnum(gpextrefnum);
+	}
+	
+	@Override
+	public GPRequest addGpRequest(GPRequest gpRequest) {
+		return gpRequestRepo.save(gpRequest);
+		
+	}
+
+	@Override
+	public GPRequest updateGpRequest(GPRequest gpRequest) {
+		return null;
+	}
+
+	@Override
+	public void deleteGpRequest(int gpRequestId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
